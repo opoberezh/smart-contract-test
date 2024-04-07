73d64a093b1ebeb49dfc3cd91f39ed2ac7dd6f07
Olha Poberezhna <124092276+opoberezh@users.noreply.github.com>
2024-04-07 21:53:47 +0200

    changes in contracts

--- contracts/ElectricToken/SolarGreenToken.sol
+++ contracts/ElectricToken/SolarGreenToken.sol
@@ -6,40 +6,41 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 //address SolarGreenToken = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
 
-
 // Contract for Soler Green Token it used with  the ERC20 base contract and has functionality as:
 // - add address to and remove address from blacklist;
 // - add and burn tokens;
 
-
-
- //interface of the contract USDT
- interface IUSDT {
+//interface of the contract USDT
+interface IUSDT {
     function mint(address account, uint256 amount) external;
- }
-
+}
 
 contract SolarGreenToken is ERC20, Ownable {
-  
-//address of the USDT contract
-address public usdtContractAddress = 0x1531BC5dE10618c511349f8007C08966E45Ce8ef;
+    // //address of the USDT contract
+    // address public usdtContractAddress =
+    //     0x1531BC5dE10618c511349f8007C08966E45Ce8ef;
 
-//interface for connecting with USDT contract
-IUSDT public usdtContract = IUSDT(usdtContractAddress);
+    // //interface for connecting with USDT contract
+    // IUSDT public usdtContract = IUSDT(usdtContractAddress);
 
-//to call function of mint  USDT contract
-function mintUSDT(uint256 amount) external onlyOwner {
-    usdtContract.mint(address(this), amount);//calling
-}
+    // //to call function of mint  USDT contract
+    // function mintUSDT(uint256 amount) external onlyOwner {
+    //     usdtContract.mint(address(this), amount); //calling
+    // }
 
     mapping(address => bool) private _blacklist; //mapping to store the blacklist status for each address;
 
     constructor(uint256 initialSupply) ERC20("Solar Green", "SGR") Ownable() {
-        _mint(msg.sender, initialSupply * 10 ** uint256(decimals()));
-    } // Constructor function to initialize the Solar Green token with the specified initial supply.
-     // It calls the constructor of the ERC20 base contract to set the name and symbol of the token,
-     // and then mints the initial supply to the deployer's address.
+        _mint(msg.sender, initialSupply * (10 ** uint256(ERC20.decimals())));
+    }
 
+    // Constructor function to initialize the Solar Green token with the specified initial supply.
+    // It calls the constructor of the ERC20 base contract to set the name and symbol of the token,
+    // and then mints the initial supply to the deployer's address.
+
+    function decimals() public pure override returns (uint8) {
+        return 18;
+    }
 
     function addToBlacklist(address account) external onlyOwner {
         _blacklist[account] = true;
@@ -53,18 +54,15 @@ function mintUSDT(uint256 amount) external onlyOwner {
         return _blacklist[account];
     } // Public function to check if a specified account is blacklisted. Retuns true or false.
 
-
     function mint(address account, uint256 amount) external onlyOwner {
         require(account != address(0), "ERC20: mint to the zero address");
         _mint(account, amount);
-    }  // Mint new tokens and assign them to the specified account.
-      // Only the owner of the contract can call this function.
-      // Reverts if the specified account is the zero address.
-
+    } // Mint new tokens and assign them to the specified account.
+    // Only the owner of the contract can call this function.
+    // Reverts if the specified account is the zero address.
 
     function burn(uint256 amount) external {
         _burn(msg.sender, amount);
-    }  // Burn a specified amount of tokens from the sender's balance.
-      // This operation permanently removes tokens from circulation.
-
+    } // Burn a specified amount of tokens from the sender's balance.
+    // This operation permanently removes tokens from circulation.
 }
--- test/SolarGreenToken-test.js
+++ test/SolarGreenToken-test.js
@@ -1,40 +1,57 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-const solarJSON = require("../artifacts/contracts/ElectricToken/SolarGreenToken.sol/SolarGreenToken.json");
 
 describe("SolarGreenToken", function () {
   let owner;
   let addr1;
   let addr2;
   let store;
-  let erc20;
+  let initialSupply;
 
   beforeEach(async function () {
     //get objs Signer for owner and addr1, and addr2
     [owner, addr1, addr2] = await ethers.getSigners();
 
     //get the contract Factory SolarGreenToken
-    const SolarGreenToken = await ethers.getContractFactory("SolarGreenToken");
+    const SolarGreenToken = await ethers.getContractFactory(
+      "SolarGreenToken",
+      owner
+    );
 
     //deploy of SGT contract
-    store = await SolarGreenToken.deploy(owner.address);
+    initialSupply = 1000000;
+    store = await SolarGreenToken.deploy(initialSupply);
 
     //wait for contract deploying and get the deployed contract
-    await store.deployed();
-
-    erc20 = new ethers.Contract(store.address, solarJSON.abi, owner);
+    // store = await store.deployed();
   });
 
   it("Should mint tokens to contract address", async function () {
     expect(await store.owner()).to.equal(owner.address);
     expect(await store.symbol()).to.equal("SGR");
-    const initialContractBalance = await store.balanceOf(store.address);
-    expect(initialContractBalance).to.equal(0); //check if balance of tokens equal 0
+
+    console.log(
+      "Balance of owner:",
+      (await store.balanceOf(owner.address)).toString()
+    );
+
+    const initialContractBalance = await store.balanceOf(owner.address);
+
+    console.log("Initial contract balance:", initialContractBalance.toString());
+
+    // console.log("ethers.utils:", ethers.utils);
+
+    // const balance = ethers.utils.formatUnits(
+    //   initialContractBalance.toString(),
+    //   18
+    // );
+
+    expect(initialContractBalance).to.equal(initialSupply); //check if balance of tokens equal initial supply
 
     const amountToMint = 500;
     await store.mint(store.address, amountToMint); // to call func mint to add 500 tokens
 
-    const finalContractBalance = await store.balanceOf(await store.address);
+    const finalContractBalance = await store.balanceOf(store.address);
 
     expect(finalContractBalance).to.equal(amountToMint); //check if balance of SGT contract = amountToMint
   });
@@ -42,28 +59,28 @@ describe("SolarGreenToken", function () {
   it("Should add and remove address from blacklist", async function () {
     //add addr to blacklist
     await store.addToBlacklist(addr1.address);
-    expect(await store.isBlacklist(addr1.address).to.equal(true));
+    expect(await store.isBlacklisted(addr1.address)).to.equal(true);
 
     //remove address from blacklist
     await store.removeFromBlacklist(addr1.address);
-    expect(await store.isBlacklist(addr1.address)).to.equal(false);
+    expect(await store.isBlacklisted(addr1.address)).to.equal(false);
   });
 
   it("Should work correctly for different addresses", async function () {
     // Add addr1 to blacklist
     await store.addToBlacklist(addr1.address);
-    expect(await store.isBlacklist(addr1.address)).to.equal(true);
+    expect(await store.isBlacklisted(addr1.address)).to.equal(true);
 
     // Add addr2 to blacklist
     await store.addToBlacklist(addr2.address);
-    expect(await store.isBlacklist(addr2.address)).to.equal(true);
+    expect(await store.isBlacklisted(addr2.address)).to.equal(true);
 
     // Remove addr1 from blacklist
     await store.removeFromBlacklist(addr1.address);
-    expect(await store.isBlacklist(addr1.address)).to.equal(false);
+    expect(await store.isBlacklisted(addr1.address)).to.equal(false);
 
     // Ensure addr2 is still in the blacklist
-    expect(await store.isBlacklist(addr2.address)).to.equal(true);
+    expect(await store.isBlacklisted(addr2.address)).to.equal(true);
   });
 
   it("should allow only owner to add or remove addresses from blacklist", async function () {
@@ -77,6 +94,124 @@ describe("SolarGreenToken", function () {
       store.connect(addr1).removeFromBlacklist(addr2.address)
     ).to.be.revertedWith("Ownable: caller is not the owner");
   });
+  const { expect } = require("chai");
+  const { ethers } = require("hardhat");
+
+  describe("SolarGreenToken", function () {
+    let owner;
+    let addr1;
+    let addr2;
+    let store;
+    let initialSupply;
+
+    beforeEach(async function () {
+      //get objs Signer for owner and addr1, and addr2
+      [owner, addr1, addr2] = await ethers.getSigners();
+
+      //get the contract Factory SolarGreenToken
+      const SolarGreenToken = await ethers.getContractFactory(
+        "SolarGreenToken",
+        owner
+      );
+
+      //deploy of SGT contract
+      initialSupply = ethers.utils.parseEther("1000000");
+      store = await SolarGreenToken.deploy(initialSupply);
+
+      //wait for contract deploying and get the deployed contract
+      // store = await store.deployed();
+    });
+
+    it("Should mint tokens to contract address", async function () {
+      expect(await store.owner()).to.equal(owner.address);
+      expect(await store.symbol()).to.equal("SGR");
+
+      console.log("Balance of owner:", await store.balanceOf(owner.address));
+
+      const initialContractBalance = await store.balanceOf(owner.address);
+
+      console.log(
+        "Initial contract balance:",
+        initialContractBalance.toString()
+      );
+
+      console.log("ethers.utils:", ethers.utils);
+
+      const balance = ethers.utils.formatUnits(
+        initialContractBalance.toString(),
+        18
+      );
+
+      expect(balance).to.equal(initialSupply); //check if balance of tokens equal initial supply
+
+      const amountToMint = 500;
+      await store.mint(store.address, amountToMint); // to call func mint to add 500 tokens
+
+      const finalContractBalance = await store.balanceOf(store.address);
+
+      expect(finalContractBalance).to.equal(amountToMint); //check if balance of SGT contract = amountToMint
+    });
+
+    it("Should add and remove address from blacklist", async function () {
+      //add addr to blacklist
+      await store.addToBlacklist(addr1.address);
+      expect(await store.isBlacklisted(addr1.address)).to.equal(true);
+
+      //remove address from blacklist
+      await store.removeFromBlacklist(addr1.address);
+      expect(await store.isBlacklisted(addr1.address)).to.equal(false);
+    });
+
+    it("Should work correctly for different addresses", async function () {
+      // Add addr1 to blacklist
+      await store.addToBlacklist(addr1.address);
+      expect(await store.isBlacklisted(addr1.address)).to.equal(true);
+
+      // Add addr2 to blacklist
+      await store.addToBlacklist(addr2.address);
+      expect(await store.isBlacklisted(addr2.address)).to.equal(true);
+
+      // Remove addr1 from blacklist
+      await store.removeFromBlacklist(addr1.address);
+      expect(await store.isBlacklisted(addr1.address)).to.equal(false);
+
+      // Ensure addr2 is still in the blacklist
+      expect(await store.isBlacklisted(addr2.address)).to.equal(true);
+    });
+
+    it("should allow only owner to add or remove addresses from blacklist", async function () {
+      // Try to add address to blacklist as non-owner
+      await expect(
+        store.connect(addr1).addToBlacklist(addr2.address)
+      ).to.be.revertedWith("Ownable: caller is not the owner");
+
+      // Try to remove address from blacklist as non-owner
+      await expect(
+        store.connect(addr1).removeFromBlacklist(addr2.address)
+      ).to.be.revertedWith("Ownable: caller is not the owner");
+    });
+
+    it("Should mint tokens to specified address", async function () {
+      const initialBalance = await store.balanceOf(addr1.address);
+      const amountToMint = 100;
+
+      await store.connect(owner).mint(addr1.address, amountToMint);
+
+      const finalBalance = await store.balanceOf(addr1.address);
+
+      expect(finalBalance).to.equal(initialBalance.add(amountToMint));
+    });
+
+    it("Should burn tokens from sender's balance", async function () {
+      const initialBalance = await store.balanceOf(owner.address);
+      const amountToBurn = 50;
+
+      await store.burn(amountToBurn);
+
+      const finalBalance = await store.balanceOf(owner.address);
+      expect(initialBalance.sub(finalBalance)).to.equal(amountToBurn);
+    });
+  });
 
   it("Should mint tokens to specified address", async function () {
     const initialBalance = await store.balanceOf(addr1.address);
@@ -85,7 +220,8 @@ describe("SolarGreenToken", function () {
     await store.connect(owner).mint(addr1.address, amountToMint);
 
     const finalBalance = await store.balanceOf(addr1.address);
-    expect(finalBalance.sub(initialBalance)).to.equal(amountToMint);
+
+    expect(finalBalance).to.equal(initialBalance.add(amountToMint));
   });
 
   it("Should burn tokens from sender's balance", async function () {
