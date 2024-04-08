c7ea0d0f68d2c2eebf3a01ef3cce6c4060cc03c4
Olha Poberezhna <124092276+opoberezh@users.noreply.github.com>
2024-04-08 22:36:55 +0200

    deploy.js

--- deploy.js
+++ deploy.js
@@ -11,34 +11,32 @@ async function deployContracts() {
 
   console.log("Contract factories loaded successfully.");
 
-  try {
-    // Deploy SolarGreenToken contract
-    const solarGreenToken = await SolarGreenToken.deploy(1000000);
-    await solarGreenToken.deployed(); // Wait for deployment to be confirmed
-    console.log("SolarGreenToken deployed to:", solarGreenToken.address);
+  // Deploy SolarGreenToken contract
+  const solarGreenToken = await SolarGreenToken.deploy(1000000);
+  const solarGreenTokenDeployment =
+    await solarGreenToken.deployTransaction.wait(); // Wait for deployment transaction to be mined
+  console.log(
+    "SolarGreenToken deployed to:",
+    solarGreenTokenDeployment.contractAddress
+  );
 
-    // Deploy TokenSale contract
-    const saleRate = 1000; // Sale rate for tokens
-    const saleCap = 50000000; // Total sale cap (50 million tokens)
-    const duration = 5 * 7 * 24 * 3600; // 5 weeks in seconds
-    const usdtAddress = "0x1531BC5dE10618c511349f8007C08966E45Ce8ef"; // Address of the chosen stablecoin (e.g., USDT or USDC)
-    console.log("USDT Contract Address:", usdtAddress);
-    const tokenSale = await TokenSale.deploy(
-      solarGreenToken.address,
-      saleRate,
-      saleCap,
-      duration,
-      deployer.address, // Pass initialOwner as deployer address
-      usdtAddress
-    );
-    await tokenSale.deployed(); // Wait for deployment to be confirmed
-    console.log("TokenSale deployed to:", tokenSale.address);
+  // Deploy TokenSale contract
+  const saleRate = 1000; // Sale rate for tokens
+  const saleCap = 50000000; // Total sale cap (50 million tokens)
+  const duration = 5 * 7 * 24 * 3600; // 5 weeks in seconds
+  const usdtAddress = "0x1531BC5dE10618c511349f8007C08966E45Ce8ef"; // Address of the chosen stablecoin (e.g., USDT or USDC)
+  console.log("USDT Contract Address:", usdtAddress);
+  const tokenSale = await TokenSale.deploy(
+    solarGreenToken.address,
+    saleRate,
+    saleCap,
+    duration,
+    deployer.address // Pass initialOwner as deployer address
+  );
+  const tokenSaleDeployment = await tokenSale.deployTransaction.wait(); // Wait for deployment transaction to be mined
+  console.log("TokenSale deployed to:", tokenSaleDeployment.contractAddress);
 
-    console.log("Deployment completed successfully.");
-  } catch (error) {
-    console.error("Error deploying contracts:", error);
-    process.exit(1);
-  }
+  console.log("Deployment completed successfully.");
 }
 
 deployContracts().then(() => process.exit(0));
--- hardhat.config.js
+++ hardhat.config.js
@@ -9,9 +9,24 @@ const MNEMONIC = process.env.MNEMONIC;
 module.exports = {
   solidity: "0.8.0",
   networks: {
-    ropsten: {
+    sepolia: {
       url: `https://sepolia.infura.io/v3/${API_KEY}`,
       accounts: { mnemonic: MNEMONIC },
     },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+
+  external: {
+    contracts: [
+      {
+        artifacts: "./artifacts",
+        deploy: "./deploy.js",
+      },
+    ],
   },
 };
